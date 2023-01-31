53265a6bbd0f9f96f77797e5ddef9f2ac5bbf48a
Philip <philipyao19@gmail.com>
2023-01-31 14:49:12 -0500

    copied over functions from Elechouse libraries

--- cc_code/src/rfid.c
+++ cc_code/src/rfid.c
@@ -14,7 +14,7 @@
 // lot of features (don't know if we actually need). USART1/2 are fully featured.
 // USART1_TX: PA9 AF1, PB6 AF0
 // USART1_RX: PA10 AF1, PB7 AF0
-
+//***** CHANGE TO USART1 *****//
 void init_usart5()
 {
     // enable RCC clock to GPIOC and GPIOD
@@ -76,27 +76,181 @@ int read_byte(void)
     return c;
 }
 
-// not really sure what the return value should be
-int writeCommand(uint8_t * buf, int buf_len)
+/*
+ * THE FOLLOWING FUNCTIONS ARE LARGELY TAKEN FROM ELECHOUSE'S C++ LIBRARY ON
+ * GITHUB, TRANSLATED TO C SYNTAX.
+ * https://github.com/elechouse/PN532/blob/PN532_HSU/PN532/PN532.cpp
+*/
+
+
+/**
+    @brief receive data .
+    @param buf --> return value buffer.
+           len --> length expect to receive.
+           timeout --> time of reveiving
+    @retval number of received bytes, 0 means no data received.
+*/
+// pass timeout=0 to avoid timeout checks
+int8_t receive(uint8_t *buf, int len, uint16_t timeout)
 {
-    for(int i = 0; i < buf_len; i++)
-    {
-        write_byte(buf[i]);
+  int read_bytes = 0;
+  int ret;
+  //unsigned long start_millis;
+
+  while (read_bytes < len) {
+    //start_millis = millis();
+    do {
+      ret = read_byte();
+      if (ret >= 0) {
+        break;
+     }
+    } while((timeout == 0) /*|| ((millis()- start_millis ) < timeout)*/);
+
+    if (ret < 0) {
+        if(read_bytes){
+            return read_bytes;
+        }else{
+            return PN532_TIMEOUT;
+        }
+    }
+    buf[read_bytes] = (uint8_t)ret;
+    //DMSG_HEX(ret);
+    read_bytes++;
+  }
+  return read_bytes;
+}
+
+int8_t readAckFrame()
+{
+    const uint8_t PN532_ACK[] = {0, 0, 0xFF, 0, 0xFF, 0};
+    uint8_t ackBuf[sizeof(PN532_ACK)];
+
+    //DMSG("\nAck: ");
+
+    if( receive(ackBuf, sizeof(PN532_ACK), 0 /*PN532_ACK_WAIT_TIME*/) <= 0 ){
+        //DMSG("Timeout\n");
+        return PN532_TIMEOUT;
+    }
+
+    if( memcmp(ackBuf, PN532_ACK, sizeof(PN532_ACK)) ){
+        //DMSG("Invalid\n");
+        return PN532_INVALID_ACK;
     }
     return 0;
 }
-// not really sure what the return value should be
-int readResponse(uint8_t * buf, int buf_len)
+
+int8_t writeCommand(const uint8_t *header, uint8_t hlen, const uint8_t *body, uint8_t blen)
 {
-    for(int i = 0; i < buf_len; i++)
-    {
-        buf[i] = read_byte();
+
+    /** dump serial buffer
+    if(_serial->available()){
+        DMSG("Dump serial buffer: ");
     }
-    return 1;
+    while(_serial->available()){
+        uint8_t ret = _serial->read();
+        DMSG_HEX(ret);
+    }*/
+
+    command = header[0];
+
+    write_byte(PN532_PREAMBLE);
+    write_byte(PN532_STARTCODE1);
+    write_byte(PN532_STARTCODE2);
+
+    uint8_t length = hlen + blen + 1;   // length of data field: TFI + DATA
+    write_byte(length);
+    write_byte(~length + 1);         // checksum of length
+
+    write_byte(PN532_HOSTTOPN532);
+    uint8_t sum = PN532_HOSTTOPN532;    // sum of TFI + DATA
+
+    //DMSG("\nWrite: ");
+
+    for(int i = 0; i < hlen; i++) {
+        write_byte(header[i]);
+    }
+    for (uint8_t i = 0; i < hlen; i++) {
+        sum += header[i];
+
+        //DMSG_HEX(header[i]);
+    }
+
+    for(int j = 0; j < blen; j++) {
+        write_byte(body, blen);
+    }
+    for (uint8_t i = 0; i < blen; i++) {
+        sum += body[i];
+
+        DMSG_HEX(body[i]);
+    }
+
+    uint8_t checksum = ~sum + 1;            // checksum of TFI + DATA
+    write_byte(checksum);
+    write_byte(PN532_POSTAMBLE);
+
+    return readAckFrame();
 }
 
-// from Elechouse Github
-// https://github.com/elechouse/PN532/blob/PN532_HSU/PN532/PN532.cpp
+
+int16_t readResponse(uint8_t * buf, uint8_t len, uint16_t timeout)
+{
+    uint8_t tmp[3];
+
+    //DMSG("\nRead:  ");
+
+    /** Frame Preamble and Start Code */
+    if(receive(tmp, 3, timeout)<=0){
+        return PN532_TIMEOUT;
+    }
+    if(0 != tmp[0] || 0!= tmp[1] || 0xFF != tmp[2]){
+        //DMSG("Preamble error");
+        return PN532_INVALID_FRAME;
+    }
+
+    /** receive length and check */
+    uint8_t length[2];
+    if(receive(length, 2, timeout) <= 0){
+        return PN532_TIMEOUT;
+    }
+    if( 0 != (uint8_t)(length[0] + length[1]) ){
+        //DMSG("Length error");
+        return PN532_INVALID_FRAME;
+    }
+    length[0] -= 2;
+    if( length[0] > len){
+        return PN532_NO_SPACE;
+    }
+
+    /** receive command byte */
+    uint8_t cmd = command + 1;               // response command
+    if(receive(tmp, 2, timeout) <= 0){
+        return PN532_TIMEOUT;
+    }
+    if( PN532_PN532TOHOST != tmp[0] || cmd != tmp[1]){
+        //DMSG("Command error");
+        return PN532_INVALID_FRAME;
+    }
+
+    if(receive(buf, length[0], timeout) != length[0]){
+        return PN532_TIMEOUT;
+    }
+    uint8_t sum = PN532_PN532TOHOST + cmd;
+    for(uint8_t i=0; i<length[0]; i++){
+        sum += buf[i];
+    }
+
+    /** checksum and postamble */
+    if(receive(tmp, 2, timeout) <= 0){
+        return PN532_TIMEOUT;
+    }
+    if( 0 != (uint8_t)(sum + tmp[0]) || 0 != tmp[1] ){
+        //DMSG("Checksum error");
+        return PN532_INVALID_FRAME;
+    }
+
+    return length[0];
+}
+
 // input arg 'reg' is the 16 bit register address
 // returns register value
 int readRegister(uint16_t reg)
@@ -112,7 +266,7 @@ int readRegister(uint16_t reg)
     }
 
     // read data packet
-    int16_t status = readResponse(pn532_packetbuffer, sizeof(pn532_packetbuffer));
+    int16_t status = readResponse(pn532_packetbuffer, sizeof(pn532_packetbuffer), 0);
     if (0 > status) {
         return 0;
     }
@@ -140,9 +294,68 @@ int writeRegister(uint16_t reg, uint8_t val)
     }
 
     // read data packet
-    int16_t status = readResponse(pn532_packetbuffer, sizeof(pn532_packetbuffer));
+    int16_t status = readResponse(pn532_packetbuffer, sizeof(pn532_packetbuffer), 0);
     if (0 > status) {
         return 0;
+    }
+
+    return 1;
+}
+
+/**************************************************************************/
+/*!
+    Waits for an ISO14443A target to enter the field
+    @param  cardBaudRate  Baud rate of the card
+    @param  uid           Pointer to the array that will be populated
+                          with the card's UID (up to 7 bytes)
+    @param  uidLength     Pointer to the variable that will hold the
+                          length of the card's UID.
+    @returns 1 if everything executed properly, 0 for an error
+*/
+/**************************************************************************/
+int readPassiveTargetID(uint8_t cardbaudrate, uint8_t *uid, uint8_t *uidLength, uint16_t timeout)
+{
+    pn532_packetbuffer[0] = PN532_COMMAND_INLISTPASSIVETARGET;
+    pn532_packetbuffer[1] = 1;  // max 1 cards at once (we can set this to 2 later)
+    pn532_packetbuffer[2] = cardbaudrate;
+
+    if (writeCommand(pn532_packetbuffer, 3)) {
+        return 0x0;  // command failed
+    }
+
+    // read data packet
+    if (readResponse(pn532_packetbuffer, sizeof(pn532_packetbuffer), 0) < 0) {
+        return 0x0;
+    }
+
+    // check some basic stuff
+    /* ISO14443A card response should be in the following format:
+      byte            Description
+      -------------   ------------------------------------------
+      b0              Tags Found
+      b1              Tag Number (only one used in this example)
+      b2..3           SENS_RES
+      b4              SEL_RES
+      b5              NFCID Length
+      b6..NFCIDLen    NFCID
+    */
+
+    if (pn532_packetbuffer[0] != 1)
+        return 0;
+
+    uint16_t sens_res = pn532_packetbuffer[2];
+    sens_res <<= 8;
+    sens_res |= pn532_packetbuffer[3];
+
+    /*DMSG("ATQA: 0x");  DMSG_HEX(sens_res);
+    DMSG("SAK: 0x");  DMSG_HEX(pn532_packetbuffer[4]);
+    DMSG("\n");*/
+
+    /* Card appears to be Mifare Classic */
+    *uidLength = pn532_packetbuffer[5];
+
+    for (uint8_t i = 0; i < pn532_packetbuffer[5]; i++) {
+        uid[i] = pn532_packetbuffer[6 + i];
     }
 
     return 1;
--- cc_code/src/rfid.h
+++ cc_code/src/rfid.h
@@ -11,6 +11,8 @@
 // buffer to hold values to send/receive to/from the PN532
 uint8_t pn532_packetbuffer[64];
 
+uint8_t command;
+
 // function declarations
 void init_usart5();
 int write_byte(int c);
@@ -60,5 +62,25 @@ int writeRegister(uint16_t reg, uint8_t val);
 
 
 #define PN532_MIFARE_ISO14443A              (0x00)
+
+// from PN532Interface
+#define PN532_PREAMBLE                (0x00)
+#define PN532_STARTCODE1              (0x00)
+#define PN532_STARTCODE2              (0xFF)
+#define PN532_POSTAMBLE               (0x00)
+
+#define PN532_HOSTTOPN532             (0xD4)
+#define PN532_PN532TOHOST             (0xD5)
+
+#define PN532_ACK_WAIT_TIME           (10)  // ms, timeout of waiting for ACK
+
+#define PN532_INVALID_ACK             (-1)
+#define PN532_TIMEOUT                 (-2)
+#define PN532_INVALID_FRAME           (-3)
+#define PN532_NO_SPACE                (-4)
+
+#define REVERSE_BITS_ORDER(b)         b = (b & 0xF0) >> 4 | (b & 0x0F) << 4; \
+                                      b = (b & 0xCC) >> 2 | (b & 0x33) << 2; \
+                                      b = (b & 0xAA) >> 1 | (b & 0x55) << 1
 
 #endif /* RFID_H_ */
