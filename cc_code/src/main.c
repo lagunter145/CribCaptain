5fdbf06f86b14c78a7765996f063f1665583303e
Philip Yao <philipyao19@gmail.com>
2023-04-11 23:18:42 -0400

    added rfid init back to main

--- cc_code/src/main.c
+++ cc_code/src/main.c
@@ -8,12 +8,11 @@
   ******************************************************************************
 */
 
-
-#include "stm32f0xx.h"
-#include "lcd_7in.h"
-#include "test.h"
 #include <stdio.h>
 #include <stdint.h>
+#include "stm32f0xx.h"
+#include "lcd_7in.h"
+#include "rfid.h"
 #include "esp.h"
 #include "timer.h"
 #include "misc.h"
@@ -23,6 +22,38 @@ extern uint8_t uid_buf[7];
 int main(void)
 {
 	setup_pcb_leds();
+
+	init_usart5();
+
+	// RFID + DMA INITIALIZATION ROUTINE
+    uint32_t versiondata = 0;
+    uint32_t chip;
+    uint32_t firmware0;
+    uint32_t firmware1;
+    int success;
+
+    wakeup();
+    versiondata = getFirmwareVersion();
+    chip = (versiondata>>24) & 0xff;
+    firmware0 = versiondata>>16 & 0xff;
+    firmware1 = versiondata>>8  & 0xff;
+    printf("%d\n", versiondata);
+
+
+    // set max retry attempts
+    // setPassiveActivationRetries(0xff);
+
+    // Configure Secure Access Module to read RFID cards
+    SAMConfig();
+
+    uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+    uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+
+
+    success = readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength, 0);
+    enable_DMA1();
+
+
 	//set_pin(GPIOA, 5, 1);
 	setup_external_timesync();
 
